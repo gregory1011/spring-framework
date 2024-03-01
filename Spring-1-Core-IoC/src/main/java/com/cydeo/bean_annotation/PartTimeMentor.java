f55e3c3f2ec7f43f6c500918ad7a53acd7e27cab
grigorerosca <gregory081093@gmail.com>
2024-03-01 11:11:46 -0500

    spring IoC

---
+++ Spring-1-Core-IoC/src/main/java/com/cydeo/bean_annotation/PartTimeMentor.java
@@ -0,0 +1,12 @@
+package com.cydeo.bean_annotation;
+
+
+import org.springframework.context.annotation.Bean;
+
+public class PartTimeMentor {
+
+    public void createAccount(){
+        System.out.println("Part time mentor acc is created...");
+    }
+
+}
