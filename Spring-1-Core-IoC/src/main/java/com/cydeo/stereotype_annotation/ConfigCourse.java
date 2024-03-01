5b425a27c591e3a43057d025e04feeb26619cfb8
grigorerosca <gregory081093@gmail.com>
2024-03-01 13:04:54 -0500

    stereotype annotation

---
+++ Spring-1-Core-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -0,0 +1,13 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+//@ComponentScan(basePackages = "com.cydeo")
+public class ConfigCourse {
+
+
+}
