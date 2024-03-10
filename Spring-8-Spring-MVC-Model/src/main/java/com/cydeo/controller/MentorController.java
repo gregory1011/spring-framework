82947210f567500f3953fcd8ef247d340a962171
grigorerosca <gregory081093@gmail.com>
2024-03-10 11:00:55 -0400

    Spring MVC model

---
+++ Spring-8-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -0,0 +1,29 @@
+package com.cydeo.controller;
+
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+  @RequestMapping("/list") // localhost:8080/mentor/list
+  public String mentors(Model model){
+
+      List<Mentor> mentorList = new ArrayList<>();
+      mentorList.add(new Mentor("Mike", "Shale",45, Gender.Male));
+      mentorList.add(new Mentor("Tom", "Hanks",65, Gender.Male));
+      mentorList.add(new Mentor("Ana", "Armors",25, Gender.Female));
+
+      model.addAttribute("mentors", mentorList);
+
+      return "mentor/mentor";
+    }
+
+}
---
+++ Spring-8-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,40 @@
+package com.cydeo.controller;
+
+import com.cydeo.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+   @RequestMapping("/welcome")
+    public String homePage(Model model){
+
+       model.addAttribute("name", "Cydeo");
+       model.addAttribute("course", "MVC");
+
+       String subject = "Collections";
+       model.addAttribute("subject", subject);
+
+       // create some random student ID and show it in UI
+       Integer id = 13;
+       model.addAttribute("id", id);
+
+       List<Integer> numbers = new ArrayList<>();
+       numbers.add(4);
+       numbers.add(5);
+       numbers.add(7);
+       numbers.add(9);
+       model.addAttribute("numbers", numbers);
+
+      Student student = new Student(13, "Leo", "Messi");
+      model.addAttribute("student", student);
+
+        return "student/welcome";
+    }
+
+}
