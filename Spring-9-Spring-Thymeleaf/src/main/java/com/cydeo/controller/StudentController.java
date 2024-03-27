b95a29804b0b804c00061ce7d2162c1a047f0f6a
grigorerosca <gregory081093@gmail.com>
2024-03-26 23:43:01 -0400

    Thymeleaf

--- Spring-9-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-9-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,8 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student") // port number 9090
@@ -19,7 +18,8 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome")
+    //@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    @GetMapping("/welcome")
     public String info(@RequestParam Integer id){
 
         System.out.println("id ="+id);
@@ -27,7 +27,8 @@ public class StudentController {
         return "student/welcome";
     }
 
-    @RequestMapping("/welcome2")
+   // @RequestMapping(value = "/welcome2", method = RequestMethod.POST)
+    @PostMapping("/welcome2")
     public String info2( @RequestParam Integer age){
 
         System.out.println("age ="+age);
