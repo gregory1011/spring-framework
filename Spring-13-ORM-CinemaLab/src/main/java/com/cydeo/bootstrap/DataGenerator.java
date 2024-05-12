a2425c620cc6427ba870bc70a8f18e811d8f742e
grigorerosca <gregory081093@gmail.com>
2024-05-12 11:42:53 -0400

    module 13 data generator

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,16 +1,25 @@
 package com.cydeo.bootstrap;
 
-import com.cydeo.repository.AccountRepo;
+import com.cydeo.entity.Genre;
+import com.cydeo.repository.*;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class DataGenerator implements CommandLineRunner {
 
     // dependency injection
-//    private final AccountRepo accountRepo;
+    private final AccountRepo accountRepo;
+    private final CinemaRepo cinemaRepo;
+    private final GenreRepo genreRepo;
+    private final MovieCinemaRepo movieCinemaRepo;
+    private final MovieRepo movieRepo;
+    private final TicketRepo ticketRepo;
+    private final UserRepo userRepo;
 
 
     @Override
@@ -18,7 +27,14 @@ public class DataGenerator implements CommandLineRunner {
 
         // ------------------- Account DERIVED QUERIES ------------------- //
 
+//        List<Genre> comedy = genreRepo.getGenreByName("Comedy");
+//        System.out.println("comedy = " + comedy);
 
+            List<Genre> genreById = genreRepo.getGenreById(1);
+            System.out.println("genreById = " + genreById);
+
+
+        // accountRepo.findByAgeLessThanEqual(40).forEach(System.out::println);
         // ------------------- Account JPQL QUERIES ------------------- //
 
 
