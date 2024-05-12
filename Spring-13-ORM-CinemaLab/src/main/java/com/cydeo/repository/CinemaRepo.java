f2efc3dc498271f19a10172f0f845bd3050a93ac
grigorerosca <gregory081093@gmail.com>
2024-05-12 11:41:45 -0400

    module 13 Cinema Repo

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepo.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepo.java
@@ -4,9 +4,11 @@ import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CinemaRepo extends JpaRepository<Cinema, Long> {
 
 
