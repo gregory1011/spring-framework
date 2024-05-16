3b5e7abed56791057fda0e7b3868c1db5b36b6e5
grigorerosca <gregory081093@gmail.com>
2024-05-16 12:14:49 -0400

    Movie Repo

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/MovieRepo.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/MovieRepo.java
@@ -5,10 +5,12 @@ import com.cydeo.enums.MovieState;
 import com.cydeo.enums.MovieType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface MovieRepo extends JpaRepository<Movie, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
