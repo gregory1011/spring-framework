326285a769fb640920bfe5bba2a9a91d987ef26d
grigorerosca <gregory081093@gmail.com>
2024-06-09 09:39:07 -0400

    Cinema

---
+++ Spring-17-REST-ConsumingAPI/src/main/java/com/cydeo/entity/Cinema.java
@@ -0,0 +1,19 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cinema extends BaseEntity {
+
+    private String name;
+    private String sponsoredName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Location location;
+
+}
