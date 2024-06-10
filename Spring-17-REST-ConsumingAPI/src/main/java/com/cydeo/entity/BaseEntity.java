28ba184e702d5e788cf27a86ca79b088fac91dcb
grigorerosca <gregory081093@gmail.com>
2024-06-10 10:02:53 -0400

    Base Entity

---
+++ Spring-17-REST-ConsumingAPI/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
