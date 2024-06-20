23636052beb7cb66ac169b647e5c654be665374a
grigorerosca <gregory081093@gmail.com>
2024-06-19 21:38:40 -0400

    Course Controller Response Entity

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -2,12 +2,15 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/courses/api/v2")
 public class CourseController_ResponseEntity {
@@ -15,9 +18,6 @@ public class CourseController_ResponseEntity {
     // this class purpose is to include certain information that my request can take and expect a certain response accordingly
     private final CourseService courseService;
 
-    public CourseController_ResponseEntity(CourseService courseService) {
-        this.courseService = courseService;
-    }
 
     @GetMapping
     public ResponseEntity<List<CourseDTO>> getAllCourses(){
@@ -31,6 +31,14 @@ public class CourseController_ResponseEntity {
                 .body(courseService.getCourses());
     }
 
+    @GetMapping("rating/{rating}")
+    public ResponseEntity<List<CourseDTO>> getCourseByRating(@PathVariable("rating") Integer rating){
+
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("service", "All set")
+                .body(courseService.getCourseByRating(rating));
+    }
 
    @GetMapping("{id}")
    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") Long id){
@@ -46,6 +54,23 @@ public class CourseController_ResponseEntity {
                 .status(HttpStatus.CREATED)
                 .header("Operation", "Create")
                 .body(courseService.createCourse(course));
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<CourseDTO> updateCourse(@PathVariable("id") Integer id, @RequestBody CourseDTO course){
+
+        CourseDTO foundStudent = courseService.getCourseById(id);
+        foundStudent.setName(course.getName());
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(foundStudent);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourse(@PathVariable("id") Integer id){
+
+        ResponseEntity.status(HttpStatus.ACCEPTED)
+                        .header("Id ="+id, "was deleted");
+        courseService.deleteCourseById(id);
     }
 
 
