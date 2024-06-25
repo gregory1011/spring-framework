7ebdd37cb559b351ad011f0b62f7f7d26c6b9788
grigorerosca <gregory081093@gmail.com>
2024-06-25 14:59:41 -0400

    Security Config

--- Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/config/SecurityConfig.java
+++ Spring-19-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/config/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+
 @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
@@ -27,23 +29,30 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
                 .anyRequest()
                 .permitAll();
         http.csrf().disable();
-}
-@Autowired
+    }
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         KeycloakAuthenticationProvider keycloakAuthenticationProvider =
                 keycloakAuthenticationProvider();
         keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
                 new SimpleAuthorityMapper());
         auth.authenticationProvider(keycloakAuthenticationProvider);
-}
+    }
+
+
     @Bean
     @Override
     protected SessionAuthenticationStrategy
     sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(
                 new SessionRegistryImpl());
-}
-@Bean
+    }
+    @Bean
     public KeycloakConfigResolver KeycloakConfigResolver() {
         return new KeycloakSpringBootConfigResolver();
-} }
+}
+
+
+
+}
