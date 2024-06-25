a6dbd3b06ffed7596f2fa4b86b4498139ac5e77c
grigorerosca <gregory081093@gmail.com>
2024-06-25 14:58:19 -0400

    data sql

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,5 +1,8 @@
 -- INSERT INTO EMPLOYEE (id,name)
 --        VALUES(1,'Mike Smith');
 
-INSERT INTO students (first_name, last_name, email)
-VALUES('Mike','Smith', 'mike@cydeo.com');
+-- INSERT INTO students (first_name, last_name, email)
+-- VALUES('Mike','Smith', 'mike@cydeo.com');
+
+-- insert into teams(id, name)
+-- values (1, "Chelsea");
